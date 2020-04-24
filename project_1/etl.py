59f4b12f19b0a391cb02dfd4e04a1372ae026939
Túlio Di Sabatino <tuliosabatino@gmail.com>
2020-04-24 23:21:08 +0100

    ETL: Completed ETL using the steps tested in the ETL notebook

--- project_1/etl.py
+++ project_1/etl.py
@@ -6,38 +6,56 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """
+    Reads the JSON file in the provided filepath and adds data 
+    from each row into song and artist tables 
+
+    Args:
+        cur : PostgreSQL cursor
+        filepath (str): Path to file
+    """    
+
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = list(df[["song_id", "title", "artist_id", "year", "duration"]].values[0])
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = list(df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values[0])
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    """
+    Reads the JSON file in the provided filepath and adds data 
+    from each row into users, time and songplays tables 
+
+    Args:
+        cur : PostgreSQL cursor
+        filepath (str): Path to file
+    """    
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df["page"]=="NextSong"]
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df.ts, unit='ms') 
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [[line.timestamp(), line.hour, line.day, line.week, line.month, line.year, line.weekday_name] for line in t]
+    column_labels = ("timestamp", "hour", "day", "week", "month", "year", "weekday")
+    time_df = pd.DataFrame.from_records(time_data, columns=column_labels)
+
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,11 +74,21 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (index, pd.to_datetime(row.ts, unit='ms').timestamp(), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """
+    Retrieves all files available in the provided filepath and
+    processes the using the provided func
+
+    Args:
+        cur : PostgreSQL cursor
+        conn : Connection object with an estabilished session to a database
+        filepath (str): Path to a file or folder
+        func : Function to be used to process the file
+    """    
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -80,11 +108,18 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """
+    Main ETL function
+
+    Connects to the sparkifydb database and provides the folders 
+    containing song data and log data to process_data() to store 
+    the data into the database
+    """    
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath="data/song_data", func=process_song_file)
+    process_data(cur, conn, filepath="data/log_data", func=process_log_file)
 
     conn.close()
 
