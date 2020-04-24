1ca485780713b74c995b77eab1268186f5cb9ec6
Túlio Di Sabatino <tuliosabatino@gmail.com>
2020-04-25 00:10:43 +0100

    sql_queries: Added data validation to table creation and insertion

--- project_1/sql_queries.py
+++ project_1/sql_queries.py
@@ -8,39 +8,39 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (songplay_id varchar,
-                                                                   start_time float, 
-                                                                   user_id int, 
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (songplay_id int PRIMARY KEY,
+                                                                   start_time float REFERENCES time(start_time), 
+                                                                   user_id int REFERENCES users(user_id), 
                                                                    level varchar, 
-                                                                   song_id varchar, 
-                                                                   artist_id varchar, 
+                                                                   song_id varchar REFERENCES songs(song_id), 
+                                                                   artist_id varchar REFERENCES artists(artist_id), 
                                                                    session_id int, 
                                                                    location varchar, 
                                                                    user_agent varchar)
 """)
 
-user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id int, 
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, 
                                                            first_name varchar, 
                                                            last_name varchar, 
                                                            gender varchar, 
                                                            level varchar)
 """)
 
-song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (song_id varchar, 
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, 
                                                            title varchar, 
                                                            artist_id varchar, 
                                                            year int, 
                                                            duration float)
 """)
 
-artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (artist_id varchar, 
+artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, 
                                                                name varchar, 
                                                                location varchar, 
                                                                latitude float, 
                                                                longitude float)
 """)
 
-time_table_create = (""" CREATE TABLE IF NOT EXISTS time (start_time float, 
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time (start_time float PRIMARY KEY, 
                                                           hour int, 
                                                           day int, 
                                                           week int, 
@@ -53,22 +53,27 @@ time_table_create = (""" CREATE TABLE IF NOT EXISTS time (start_time float,
 
 songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            ON CONFLICT (songplay_id) DO NOTHING;
 """)
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
                             VALUES (%s, %s, %s, %s, %s)
+                            ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) 
                             VALUES (%s, %s, %s, %s, %s)
+                            ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) 
                             VALUES (%s, %s, %s, %s, %s)
+                            ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
                             VALUES (%s, %s, %s, %s, %s, %s, %s)
+                            ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
@@ -80,5 +85,5 @@ song_select = ("""SELECT artists.artist_id, song_id
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop, songplay_table_drop]
