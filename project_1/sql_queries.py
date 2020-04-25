b64900caa40495a0e4660323fe860ac7ce9c21c6
Túlio Di Sabatino <tuliosabatino@gmail.com>
2020-04-25 13:35:35 +0100

    Project1-sql_queries: Added ON CONFLICT rules for users and artists tables

--- project_1/sql_queries.py
+++ project_1/sql_queries.py
@@ -58,7 +58,7 @@ songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
                             VALUES (%s, %s, %s, %s, %s)
-                            ON CONFLICT (user_id) DO NOTHING;
+                            ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) 
@@ -68,7 +68,11 @@ song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, dura
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) 
                             VALUES (%s, %s, %s, %s, %s)
-                            ON CONFLICT (artist_id) DO NOTHING;
+                            ON CONFLICT (artist_id) DO UPDATE 
+                                SET name=EXCLUDED.name, 
+                                    location=EXCLUDED.location, 
+                                    latitude=EXCLUDED.latitude, 
+                                    longitude=EXCLUDED.longitude;
 """)
 
 time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
