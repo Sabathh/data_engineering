300e017b29ca1a38bdcd859c64f0959273fcbd9c
Túlio Di Sabatino <tuliosabatino@gmail.com>
2020-05-13 14:08:31 +0100

    Project5: Created operator to perform a quality check on the tables

--- project_5/airflow/plugins/operators/data_quality.py
+++ project_5/airflow/plugins/operators/data_quality.py
@@ -8,15 +8,32 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 tables=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('Data checks...')
+
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        for table in self.tables:
+            self.log.info(f'Performing data check on {table}...')
+
+            records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(
+                    f"QUALITY CHECK FAILED. {table} returned no results")
+            
+            num_records = records[0][0]
+            if num_records < 1:
+                self.log.info(f"QUALITY CHECK FAILED. No records found in table {table}!!")
+                raise ValueError(f"QUALITY CHECK FAILED. No records found in table {table}!!")
+
+            self.log.info(f"Data quality analysis on table {table} completed with {num_records} records")
+        
