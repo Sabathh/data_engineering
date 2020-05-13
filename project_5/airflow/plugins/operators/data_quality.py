5f2250adff63119c28f330d567be97a82bf76e7f
Túlio Di Sabatino <tuliosabatino@gmail.com>
2020-05-13 16:25:02 +0100

    Project5: Parameterized data quality checks

--- project_5/airflow/plugins/operators/data_quality.py
+++ project_5/airflow/plugins/operators/data_quality.py
@@ -9,31 +9,42 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 tables=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         
         self.redshift_conn_id = redshift_conn_id
-        self.tables = tables
+        self.sql_checks = kwargs['params']['sql_checks']
 
     def execute(self, context):
         self.log.info('Data checks...')
 
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
-        for table in self.tables:
-            self.log.info(f'Performing data check on {table}...')
+        for check in self.sql_checks:
+            sql = check.get('check_sql')
+            compr = check.get('comparison')
+            exp_result = check.get('expected_result')
 
-            records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
-            if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(
-                    f"QUALITY CHECK FAILED. {table} returned no results")
-            
-            num_records = records[0][0]
-            if num_records < 1:
-                self.log.info(f"QUALITY CHECK FAILED. No records found in table {table}!!")
-                raise ValueError(f"QUALITY CHECK FAILED. No records found in table {table}!!")
+            records = redshift.get_records(sql)[0]
 
-            self.log.info(f"Data quality analysis on table {table} completed with {num_records} records")
+            failing_tests = []
+            error_count = 0
+
+            if compr == 'eq':                
+                if exp_result != records[0]:
+                    error_count += 1
+                    failing_tests.append(sql)
+                    
+            if compr == 'gt':
+                if records[0] < exp_result:
+                    error_count += 1
+                    failing_tests.append(sql)
+
+
+        if error_count > 0:
+            self.log.info('The following data quality checks have failed: ')
+            self.log.info(failing_tests)
+            raise ValueError('Data quality check failed')
+
         
