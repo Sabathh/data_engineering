bc248d33ef02c16a29159459a2c0d5fd11d10e1e
Túlio Di Sabatino <tuliosabatino@gmail.com>
2020-05-13 14:02:16 +0100

    Project5: Updated table insert statements with correct parameters

--- project_5/airflow/plugins/helpers/sql_queries.py
+++ project_5/airflow/plugins/helpers/sql_queries.py
@@ -1,41 +1,71 @@
 class SqlQueries:
     songplay_table_insert = ("""
-        SELECT
-                md5(events.sessionid || events.start_time) songplay_id,
-                events.start_time, 
-                events.userid, 
-                events.level, 
-                songs.song_id, 
-                songs.artist_id, 
-                events.sessionid, 
-                events.location, 
-                events.useragent
-                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
-            FROM staging_events
-            WHERE page='NextSong') events
+        INSERT INTO songplays (
+        start_time,
+        userid,
+        level,
+        songid,
+        artistid,
+        sessionid,
+        location,
+        user_agent,
+        playid)
+        SELECT DISTINCT
+                TIMESTAMP 'epoch' + events.ts/1000 * interval '1 second'
+                AS start_time,
+                events.userid,
+                events.level,
+                songs.song_id,
+                songs.artist_id,
+                events.sessionid,
+                events.location,
+                events.useragent,
+                md5(events.sessionid || start_time) AS playid
+            FROM staging_events events
             LEFT JOIN staging_songs songs
             ON events.song = songs.title
+            WHERE events.page = 'NextSong'
                 AND events.artist = songs.artist_name
                 AND events.length = songs.duration
+                AND events.userid IS NOT NULL
+                AND events.level IS NOT NULL
+                AND songs.song_id IS NOT NULL
+                AND songs.artist_id IS NOT NULL
+                AND events.sessionid IS NOT NULL
+                AND events.location IS NOT NULL
+                AND events.useragent IS NOT NULL
     """)
 
     user_table_insert = ("""
+        INSERT INTO users (userid, first_name, last_name, gender, level)
         SELECT distinct userid, firstname, lastname, gender, level
         FROM staging_events
-        WHERE page='NextSong'
+        WHERE page='NextSong' AND userid is not NULL
     """)
 
     song_table_insert = ("""
+        INSERT INTO songs (songid, title, artistid, year, duration)
         SELECT distinct song_id, title, artist_id, year, duration
         FROM staging_songs
     """)
 
     artist_table_insert = ("""
-        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+        INSERT INTO artists (
+        artistid,
+        name,
+        location,
+        latitude,
+        longitude)
+        SELECT distinct artist_id,
+        artist_name,
+        artist_location,
+        artist_latitude,
+        artist_longitude
         FROM staging_songs
     """)
 
     time_table_insert = ("""
+        INSERT INTO time (start_time, hour, day, week, month, year, weekday)
         SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
                extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
         FROM songplays
